fb7f8e6bb46bea382cbe5b3b1e4e5f3fc86ec7b9
Harish <harishbarodiya111@gmail.com>
2021-02-02 14:26:16 +0530

    Sidebar added

---
+++ Project/js/script.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function(){
+$('.counter-count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 5000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+});
